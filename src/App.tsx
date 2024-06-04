6d68207a318c9322323856e86f82ae184ef26dc7
BdeesornR <bdeesorn@gmail.com>
2024-06-04 21:47:12 +0700

    Add default API

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import { useState } from 'react';
+import Axios from 'axios';
+import { Button, Form, Input, Space } from 'antd';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnClick = (evt: React.MouseEvent<HTMLElement>) => {
+    Axios.post("http://localhost:8080/users/sign-up", {
+      username: username,
+      email: "test@custom_mail.com",
+      password: password
+    }, {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    }).then(res => {
+      console.log("res", res);
+    }).catch(err => {
+      console.log("err", err);
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Form
+          name="log-in-form"
+          layout="vertical"
         >
-          Learn React
-        </a>
+          <Space className="d-flex">
+            <Form.Item
+              layout="vertical"
+              label="Username"
+            >
+              <Input id="username" name="username" value={username} placeholder="Username" onChange={e => setUsername(e.target.value)} />
+            </Form.Item>
+            <Form.Item
+              layout="vertical"
+              label="Password"
+            >
+              <Input id="password" name="password" value={password} placeholder="Password" onChange={e => setPassword(e.target.value)} />
+            </Form.Item>
+          </Space>
+          <Form.Item>
+            <Button type="primary" onClick={handleOnClick}>Save</Button>
+          </Form.Item>
+        </Form>
       </header>
     </div>
   );
